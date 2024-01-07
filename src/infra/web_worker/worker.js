1d9a6ee83f616b195c1df6c24af4fc95d1fbf42b
João Santos <josan5368@gmail.com>
2024-01-07 19:38:04 +0100

    chore: update web worker

--- src/infra/web_worker/worker.js
+++ src/infra/web_worker/worker.js
@@ -4,36 +4,30 @@ import { Cron } from "deps";
 const jobs = new Map();
 
 onmessage = (event) => {
-  const { type, alert } = event.data;
+  const { type, payload } = event.data;
 
-  if (type === CronType.PUBLISH) {
-    const seconds = alert.time.getSeconds();
-    const rate = alert.repeatEvery.value;
+  if (type === CronType.PUBLISH) { 
+            
+    const seconds = payload.time.getSeconds()
+    
     const job = new Cron(`${seconds} * * * * *`, {
-      interval: rate,
+      interval: payload.rate,
       timezone: "Africa/Luanda",
-      startAt: alert.time.toISOString(),
+      startAt: payload.time.toISOString(),
     });
-    const payload = {
-      patient: {
-        patientId: alert.patient.patientId.value,
-        name: alert.patient.name,
-      },
-      alertId: alert.alertId.value,
-      parameters: alert.parameters,
-      comments: alert.comments,
-      repeatEvery: alert.repeatEvery,
-      time: alert.time,
-    };
 
     job.schedule(() => postMessage(payload));
-    jobs.set(alert.alertId.value, job);
+    jobs.set(payload.alertId, job);
   }
 
   if (type === CronType.REMOVE) {
-    const job = jobs.get(alert.alertId.value);
-    if (job) {
-      job.stop();
-    }
+    removeCron(payload.alertId)
   }
 };
+
+function removeCron(alertId) {
+  const job = jobs.get(alertId);
+  if (job) {
+    job.stop();
+  }
+}
