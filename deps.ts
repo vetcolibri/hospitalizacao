f0857b67550a37ec85d0cbae59c252fe2902c17c
João Santos <josan5368@gmail.com>
2024-01-09 17:13:17 +0100

    update: changed version of croner lib

--- deps.ts
+++ deps.ts
@@ -3,6 +3,6 @@ export type { ResponseBody } from "https://deno.land/x/oak@v12.6.1/response.ts";
 export type { Next } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export { z } from "https://deno.land/x/zod@v3.22.2/mod.ts";
-export { Cron } from "https://deno.land/x/croner@6.0.3/dist/croner.js";
+export { Cron } from "https://deno.land/x/croner@8.0.0/dist/croner.js";
 export { DB } from "https://deno.land/x/sqlite@v3.8/mod.ts";
 export type { RowObject, SqliteOptions } from "https://deno.land/x/sqlite@v3.8/mod.ts";
