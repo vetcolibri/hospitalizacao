4c3f748abfb6f7f5ff183931ec9893ae29609cbd
jocaetano1 <josan5368@gmail.com>
2024-11-22 03:39:56 +0100

    refactor: remove verify token endpoint

--- src/infra/http/auth_router.ts
+++ src/infra/http/auth_router.ts
@@ -2,7 +2,7 @@ import { AuthService } from "application/auth_service.ts";
 import { Context, Router } from "deps";
 import { sendOk, sendServerError, sendUnauthorized } from "infra/http/responses.ts";
 import { validate } from "shared/tools.ts";
-import { loginSchema, validateTokenSchema } from "infra/http/schemas/user_schema.ts";
+import { loginSchema } from "infra/http/schemas/user_schema.ts";
 
 export default function(service: AuthService) {
     const loginHandler = async (ctx: Context) => {
@@ -24,26 +24,7 @@ export default function(service: AuthService) {
         }
     }
 
-    const verifyTokenHandler = async (ctx: Context) => {
-        const { token } = ctx.state.validatedData;
-
-        try {
-            const userOrErr = await service.verifyToken(token)
-
-            if (userOrErr.isLeft()) {
-                sendUnauthorized(ctx)
-                return
-            }
-
-            sendOk(ctx, userOrErr.value)
-
-        } catch (error) {
-            sendServerError(ctx, error)
-        }
-    }
-
     const router = new Router({ prefix: "/auth" });
     router.post("/login",  validate(loginSchema), loginHandler)
-    router.post("/verify-token",  validate(validateTokenSchema), verifyTokenHandler)
     return router
 }
